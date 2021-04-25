0df7a6852fe451d965c0112a410a6e1e5674c9d4
farrej10 <farrej10@tcd.ie>
2021-04-25 21:30:41 +0100

    bubble sort completed

--- js/bars.js
+++ js/bars.js
@@ -10,7 +10,6 @@ const green = "#00FF00"
 function wait(ms) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            console.log("Done waiting");
             resolve(ms)
         }, ms)
     })
@@ -37,7 +36,7 @@ class Bar {
 class Graph {
     constructor(ctx, numberofNodes, delta) {
         this.bars = []
-        var offset = 0
+        var offset = 5
         this.delta = delta
         for (var i = 0; i < numberofNodes; i++) {
             this.bars.push(new Bar(ctx, offset + i * 10, 0, 10, 20 + i * 5, black))
@@ -65,8 +64,17 @@ class Graph {
             temporaryValue = this.bars[currentIndex].height;
             this.bars[currentIndex].height = this.bars[randomIndex].height;
             this.bars[randomIndex].height = temporaryValue;
+            this.bars[currentIndex].color = black
         }
 
+    }
+    isSorted() {
+        for (var i = 0; i < this.bars.length - 1; i++) {
+            if (this.bars[i].height > this.bars[i + 1].height) {
+                return false;
+            }
+        }
+        return true
     }
     async selectionSort() {
         let n = this.bars.length;
@@ -102,30 +110,76 @@ class Graph {
                 this.bars[min].height = tmp;
 
                 this.bars[i].color = green
-                await wait(this.delta)
                 this.draw()
 
             }
             else if (min == i) {
                 this.bars[i].color = green
-                await wait(this.delta)
                 this.draw()
             }
 
         }
         this.draw();
     }
+    async bubbleSort() {
+        let n = this.bars.length;
+        let swapped = false;
+
+        for (let i = 0; i < n - 1; i++) {
+            for (let j = 0; j < n - i - 1; j++) {
+
+
+                this.bars[j].color = red
+                this.bars[j+1].color = red
+
+
+
+                this.draw()
+                await wait(this.delta)
+
+                this.bars[j].color = black
+                this.bars[j+1].color = black
+
+                if (this.bars[j].height > this.bars[j + 1].height) {
+                    let tmp = this.bars[j].height;
+                    this.bars[j].height = this.bars[j + 1].height;
+                    this.bars[j + 1].height = tmp;
+                    swapped = true
+                }
+            }
+
+            this.bars[n-i-1].color = green
+            if (!swapped) {
+                break;
+            }
+        }
+        this.bars[0].color = green
+        this.draw();
+    }
 }
 
 
 
-(async () => {
-    try {
-        graph = new Graph(ctx, 40,10)
-        graph.shuffle()
-        graph.draw()
-        await graph.selectionSort()
-    } catch (e) {
-        console.log(e)
+
+graph = new Graph(ctx, 54, 5)
+graph.shuffle()
+graph.draw()
+
+document.getElementById("shuffle").onclick = function () {
+    graph = new Graph(ctx, 54, 5)
+    graph.shuffle()
+    graph.draw()
+};
+// document.getElementById("sort").onclick = async function () {
+//     if (!graph.isSorted()) {
+//         await graph.selectionSort();
+//     }
+
+// };
+document.getElementById("sort").onclick = async function () {
+    if (!graph.isSorted()) {
+        await graph.bubbleSort();
+        graph.draw();
     }
-})();
+
+};
